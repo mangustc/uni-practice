8957b0d2744c9a1e9fde4fb9717d0ae5f2c99769
GGOOODD <131172756+GGOOODD@users.noreply.github.com>
2025-02-10 18:38:24 +0700

    Revert "Day1(Confirm)"
    This reverts commit 57180ff4343df06523b4317bb0f09ab27d90bdd5.

--- backend/schemas/api/Product.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, status, Response, Request
-from schemas import *
-from services import ProductService
-
-router = APIRouter(tags=["Product"], prefix="/product")
-
-@router.post("/AddProduct")
-async def add_product(product: ProductInformation):
-    return await ProductService.add_inform_product(product)
