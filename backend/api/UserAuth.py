693c8cfaec955b41a8a074844e5f0f4ce355bda8
GGOOODD <131172756+GGOOODD@users.noreply.github.com>
2025-02-10 18:02:36 +0700

    Создал регистрацию для юр.лиц и ИП

--- backend/api/UserAuth.py
+++ backend/api/UserAuth.py
@@ -9,9 +9,21 @@ router = APIRouter(tags=["User"], prefix="/user")
 async def register(user: Registr):
     return await UserService.registration(user)
 
+
+@router.post("/register_legal_entity")
+async def register_legal_entity(user: RegistrLegalEntity):
+    return await UserService.registration_legal_entity(user)
+
+
+@router.post("/register_ip")
+async def register_ip(user: RegistrLegalEntity):
+    return await UserService.registration_ip(user)
+
+
 @router.put("/login")
 async def login(login_data: Login, response: Response):
     return await UserService.login(login_data, response)
+
 
 @router.post("/logout")
 async def logout(response: Response):
@@ -31,7 +43,6 @@ async def update_surname(surname: str, request: Request):
 @router.get("/me")
 async def get_user_info(request: Request):
     return await UserService.get_user_info(request)
-
 
 
 @router.delete("/delete_me")
