75f5738d4099defb1edff22f69cff5e147ce5ca8
Ivan Lifanov <letalbark@gmail.com>
2025-02-11 21:27:34 +0700

    add origins to frontend to fix CORS

--- backend/main.py
+++ backend/main.py
@@ -24,6 +24,10 @@ app.include_router(api_router)
 origins = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "http://127.0.0.1:80",
+    "http://localhost:80",
 ]
 
 app.add_middleware(
