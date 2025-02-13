63c33a0bd022dac999bdce9afc5d6b073236f5bd
UelWico <gizizula@gmail.com>
2025-02-13 21:07:14 +0700

    edit header

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,8 +1,8 @@
 // import { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+// import { Link, useNavigate } from "react-router-dom";
 
 export default function Header() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   // const { authenticated, setAuthenticated } = useContext(AuthContext);
   // const logout = function () {
   //   requests.PUT_logout().then(() => {
@@ -11,8 +11,18 @@ export default function Header() {
   //   });
   // };
   return (
-    <div className="navigation-container">
-      {/* <nav
+    <div className="navigation-container-container">
+      <div className="navigation-info">
+        <span className="nav-info-text">8-800-250-8045</span>
+        <div className="nav-panel-group" style={{ gap: "24px" }}>
+          <span className="nav-info-text-gray"> Оптовым покупателям</span>
+          <span className="nav-info-text-gray">О компании</span>
+          <span className="nav-info-text-gray">Мастер-классы</span>
+          <span className="nav-info-text-gray">Контакты</span>
+        </div>
+      </div>
+      <div className="navigation-container">
+        {/* <nav
         className="links-container"
         style={{
           borderBottom: "1px solid #c2c2c2",
@@ -24,47 +34,70 @@ export default function Header() {
           </button>
         </Link>
       </nav> */}
-      <div>
-        <img src="/logo.svg" alt="" />
-      </div>
-      <div className="catalog-button">
-        <img
-          src="/menu.svg"
-          alt=""
-          style={{ marginLeft: "15px", marginRight: "15px" }}
-        />
-        <p style={{ color: "white", fontWeight: 700 }}>Каталог</p>
-      </div>
-      <div className="search" style={{ width: "507px" }}>
-        <p>Поиск</p>
-        <img src="/search.svg" alt="" />
-      </div>
-      <div
-        style={{
-          width: "fit-content",
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <div className="nav-panel-icons">
-          <img src="/heart.svg" alt="" />
-          <span style={{ fontWeight: 700, fontSize: "10px", height: "12px" }}>
-            Избранное
-          </span>
+        <div>
+          <img src="/logo.svg" alt="" />
         </div>
-        <div className="nav-panel-icons">
-          <img src="/shopping-cart.svg" alt="" />
-          <span style={{ fontWeight: 700, fontSize: "10px", height: "12px" }}>
-            Корзина
-          </span>
-        </div>
-        <div className="nav-panel-icons">
-          <img src="/user.svg" alt="" />
-          <span style={{ fontWeight: 700, fontSize: "10px", height: "12px" }}>
-            Кабинет
-          </span>
+        <div className="nav-panel-group">
+          <div className="catalog-button">
+            <img
+              src="/menu.svg"
+              alt=""
+              style={{ marginLeft: "15px", marginRight: "15px" }}
+            />
+            <p style={{ color: "white", fontWeight: 700 }}>Каталог</p>
+          </div>
+          <div className="search">
+            <p style={{ color: "var(--text-color-black-main-50)" }}>
+              Поиск по каталогу
+            </p>
+            <img src="/search.svg" alt="" />
+          </div>
+          <div className="nav-panel-icons">
+            <div id="nav-heart" className="nav-panel-icon">
+              <img src="/heart.svg" alt="" />
+              <span
+                style={{ fontWeight: 700, fontSize: "10px", height: "12px" }}
+              >
+                Избранное
+              </span>
+            </div>
+            <div id="nav-cart" className="nav-panel-icon">
+              <img src="/shopping-cart.svg" alt="" />
+              <span
+                style={{ fontWeight: 700, fontSize: "10px", height: "12px" }}
+              >
+                Корзина
+              </span>
+            </div>
+            <div id="nav-user" className="nav-panel-icon">
+              <img src="/user.svg" alt="" />
+              <span
+                style={{
+                  fontWeight: 700,
+                  fontSize: "10px",
+                  height: "12px",
+                }}
+              >
+                Кабинет
+              </span>
+            </div>
+            <div
+              id="nav-login"
+              className="nav-panel-icon"
+              style={{ display: "none" }}
+            >
+              <img src="/login.svg" alt="" />
+              <span
+                style={{
+                  fontWeight: 700,
+                  fontSize: "10px",
+                  height: "12px",
+                }}
+              >
+                Вход
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
