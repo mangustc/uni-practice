0dfcbeea956197ddd81d3318167da66d25e947e1
GGOOODD <131172756+GGOOODD@users.noreply.github.com>
2025-02-14 01:30:09 +0700

    Настроено каскадное удаление

--- backend/database.py
+++ backend/database.py
@@ -2,10 +2,17 @@ from datetime import datetime
 
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base, relationship
-from sqlalchemy import Column, String, Boolean, Integer, ForeignKey, Float, Date, DateTime
+from sqlalchemy import Column, String, Boolean, Integer, ForeignKey, Float, Date, DateTime, event
+from sqlalchemy.engine import Engine
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
 engine = create_async_engine("sqlite+aiosqlite:///./data.db")
-
 new_session = async_sessionmaker(engine, expire_on_commit=False)
 
 Base = declarative_base()
@@ -23,6 +30,9 @@ class User(Base):
     organization_name = Column(String, nullable=True)
     INN = Column(Integer, nullable=True)
 
+    orders = relationship("Order", back_populates="user", cascade="all, delete", passive_deletes=True)
+    wishlist = relationship("Wishlist", back_populates="user", cascade="all, delete", passive_deletes=True)
+
 
 class Article(Base):
     __tablename__ = "article"
@@ -36,11 +46,14 @@ class Article(Base):
     measured_in = Column(String, nullable=False)  # "м", "упак", "шт"
     price = Column(Integer, nullable=False)
 
+    category = relationship("Category", back_populates="articles")
+    products = relationship("Product", back_populates="article", cascade="all, delete", passive_deletes=True)
+
 
 class Product(Base):
     __tablename__ = "product"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    article_id = Column(Integer, ForeignKey("article.id"))
+    article_id = Column(Integer, ForeignKey("article.id",  ondelete="CASCADE"), nullable=False)
     name = Column(String, nullable=False)
     image_path = Column(String, nullable=True)
     amount = Column(Float, nullable=False)
@@ -53,7 +66,10 @@ class Product(Base):
     new_price = Column(Float, nullable=True)
     purchase_count = Column(Integer, default=0)
     last_hit_date = Column(DateTime, nullable=True)
-    article = relationship("Article")
+
+    article = relationship("Article", back_populates="products")
+    wishlists = relationship("Wishlist", back_populates="product", cascade="all, delete", passive_deletes=True)
+    carts = relationship("Cart", back_populates="product", cascade="all, delete", passive_deletes=True)
 
 
 class Wishlist(Base):
@@ -61,12 +77,17 @@ class Wishlist(Base):
     user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
     product_id = Column(Integer, ForeignKey("product.id"), primary_key=True)
 
+    user = relationship("User", back_populates="wishlist")
+    product = relationship("Product", back_populates="wishlists")
+
 
 class Cart(Base):
     __tablename__ = "cart"
     user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
     product_id = Column(Integer, ForeignKey("product.id"), primary_key=True)
     amount = Column(Float, nullable=False)
+
+    product = relationship("Product", back_populates="carts")
 
 
 class Order(Base):
@@ -78,6 +99,8 @@ class Order(Base):
     payment_status = Column(String, nullable=False, default="pending")  # "pending", "paid", "failed"
     items = Column(String, nullable=True)  # JSON строка списка товаров
 
+    user = relationship("User", back_populates="orders")
+
 
 # КатегорииТоваров
 class Category(Base):
@@ -85,9 +108,9 @@ class Category(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, nullable=False)
     parent_id = Column(Integer, ForeignKey("category.id"), nullable=True)
-    # subcategories = relationship('Category', remote_side=[id], uselist=True)
 
-
+    parent = relationship("Category", remote_side=[id], backref="children", cascade="all, delete", passive_deletes=True)
+    articles = relationship("Article", back_populates="category", cascade="all, delete", passive_deletes=True)
 
 
 async def create_tables():
