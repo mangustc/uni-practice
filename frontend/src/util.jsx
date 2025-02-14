b38e1f9daf8489644b89c0fafa8c5944383474a9
Ivan Lifanov <letalbark@gmail.com>
2025-02-14 16:04:51 +0700

    front: add GetSetObjectAttributeFunc function to util to set state object attributes

--- frontend/src/util.jsx
+++ frontend/src/util.jsx
@@ -1,3 +1,13 @@
 export const DeepClone = function (obj) {
   return structuredClone(obj);
 };
+
+export const GetSetObjectAttrtibuteFunc = function (setObj) {
+  return function (key, value) {
+    setObj((obj) => {
+      const newObj = { ...obj };
+      newObj[key] = value;
+      return newObj;
+    });
+  };
+};
