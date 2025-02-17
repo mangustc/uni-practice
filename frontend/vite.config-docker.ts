b44d576248a465cc9076e21d2c6fab51b6f372df
Ivan Lifanov <letalbark@gmail.com>
2025-02-17 21:46:44 +0700

    add docker vite server configuration and update Dockerfile to auto-load it instead of the default one

--- frontend/vite.config-docker.ts
+++ frontend/vite.config-docker.ts
@@ -5,4 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   envDir: "./environments",
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
 });
