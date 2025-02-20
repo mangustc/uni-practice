b2f515d681cab09c77d9d4c04d6709e3de732124
UelWico <gizizula@gmail.com>
2025-02-20 19:45:22 +0700

    fix unused *** "error"

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -23,6 +23,13 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+        "no-unused-vars": ["warn", {
+            "vars": "all",
+            "args": "after-used",
+            "caughtErrors": "all",
+            "ignoreRestSiblings": false,
+            "reportUsedIgnorePattern": false
+        }]
     },
   },
 )
