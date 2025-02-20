619579b6181b39b060ebf4cb295af59190fd1043
UelWico <gizizula@gmail.com>
2025-02-20 18:13:37 +0700

    edit card

--- frontend/src/components/card.tsx
+++ frontend/src/components/card.tsx
@@ -1,7 +1,15 @@
 export default function Card() {
     return(
         <div className="card-container">
-            <div className="card-photo"></div>
+            <div className="card-photo">
+                <div>
+                <div className="card-filter">Хит</div>
+                <div className="card-filter">Новинка</div>
+                <div className="card-filter">Акция</div>
+                </div>
+                <div className="card-like"></div>
+                
+            </div>
             <span className="card-name">Text</span>
             <span className="card-descr">Цена, м</span>
             <span className="card-cost">14 Р</span>
