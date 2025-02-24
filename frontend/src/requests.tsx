22ca59005c1162d6e4c2b9bc264122f9d5446f8f
Ivan Lifanov <letalbark@gmail.com>
2025-02-24 20:06:04 +0700

    add changeWishlistState request

--- frontend/src/requests.tsx
+++ frontend/src/requests.tsx
@@ -50,43 +50,21 @@ export async function POST_GetProductsByCategory(
   return objects.NewProductCatalogList(json);
 }
 
-// export async function GET_GetCategoryList() {
-//   // let response = await fetch(BACKEND_URL + "/dish/get_dishes", {
-//   //   method: "POST",
-//   //   headers: HEADER_JSON,
-//   //   credentials: "include",
-//   // });
-//   return [
-//     objects.NewCategory({
-//       category_id: 1,
-//       category_name: "test",
-//     }),
-//     objects.NewCategory({
-//       category_id: 2,
-//       category_name: "test2",
-//     }),
-//     objects.NewCategory({
-//       category_id: 3,
-//       category_name: "test3",
-//       category_parent_id: 1,
-//     }),
-//     objects.NewCategory({
-//       category_id: 4,
-//       category_name: "test4",
-//       category_parent_id: 1,
-//     }),
-//     objects.NewCategory({
-//       category_id: 5,
-//       category_name: "test6",
-//       category_parent_id: 1,
-//     }),
-//     objects.NewCategory({
-//       category_id: 6,
-//       category_name: "testunder",
-//       category_parent_id: 3,
-//     }),
-//   ];
-// }
+export async function PUT_ChangeWishlistState(productID: number): Promise<{
+  message: string;
+  status: boolean;
+}> {
+  let response = await fetch(`${BACKEND_URL}/product/change_wishlist_state/${productID}`, {
+    method: "PUT",
+    headers: HEADER_JSON,
+    credentials: "include",
+  });
+
+  const json = await response.json();
+  // TODO: Handle Error
+
+  return json;
+}
 
 export async function GET_GetCart() {
   let response = await fetch(BACKEND_URL + "/cart/get_user_cart", {
