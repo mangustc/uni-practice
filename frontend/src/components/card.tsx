6b17df35dfa9912551f32f749df2e94220967cac
Ivan Lifanov <letalbark@gmail.com>
2025-02-24 20:08:01 +0700

    add card photo and wishlist state

--- frontend/src/components/card.tsx
+++ frontend/src/components/card.tsx
@@ -1,21 +1,27 @@
+import { useState } from "react";
 import * as objects from "../objects.tsx";
 import * as requests from "../requests.tsx";
 
 export default function Card({productCatalog, photoSrc}: {
     productCatalog: objects.ProductCatalog;
     photoSrc: string;
-
 }) {
+    const [wishlist, setWishlist] = useState(productCatalog.productInWishlist)
     return (
         <div className="card-container">
-            <div className="card-photo">
+            <div className="card-photo" style={{backgroundImage: `url("${photoSrc}")`}}>
                 <div>
                     {productCatalog.productHit ? <div className="card-filter">Хит</div> : null}
                     {productCatalog.productNew ? <div className="card-filter">Новинка</div> : null}
                     {productCatalog.productPromotion ? <div className="card-filter">Акция</div> : null}
                 </div>
-                <div className="card-like"></div>
-
+                <div 
+                    className={wishlist ? "card-like-wishlist" : "card-like"} 
+                    onClick={() => {
+                        requests.PUT_ChangeWishlistState(productCatalog.productID)
+                        setWishlist(!wishlist);
+                    }}
+                ></div>
             </div>
             <span className="card-name">{productCatalog.productName}</span>
             <span className="card-descr">Цена, {productCatalog.productMeasuredIn}</span>
