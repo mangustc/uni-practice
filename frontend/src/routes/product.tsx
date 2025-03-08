cd696f035b458898b34af0eb7e1fce06aa1d4fdc
Ivan Lifanov <letalbark@gmail.com>
2025-03-08 19:55:39 +0700

    fix product photos being saved to browser cache

--- frontend/src/routes/product.tsx
+++ frontend/src/routes/product.tsx
@@ -66,7 +66,7 @@ export function Product({}: {}) {
                   className="product-mini-photo"
                   style={{
                     cursor: "pointer",
-                    backgroundImage: `url("${requests.BACKEND_URL}/product/get_photo/${currentProductID}")`
+                    backgroundImage: `url("${requests.BACKEND_URL}/product/get_photo/${currentProductID}?rand=${Date.now()}")`
                   }}
                 ></div>
               </div>
@@ -76,7 +76,7 @@ export function Product({}: {}) {
                 className="product-big-photo"
                 style={{
                   cursor: "pointer",
-                  backgroundImage: `url("${requests.BACKEND_URL}/product/get_photo/${currentProductID}")`
+                  backgroundImage: `url("${requests.BACKEND_URL}/product/get_photo/${currentProductID}?rand=${Date.now()}")`
                 }}
               ></div>
               <div className="product-info-article" style={{height: "fit-content",  width: "435px", lineHeight: "15px", marginBottom: 0}}>Обращаем ваше внимание, что цвет товара может отличаться по тону, в зависимости от настроек вашего монитора</div>
@@ -91,7 +91,7 @@ export function Product({}: {}) {
                 .sort((a, b) => a.productID - b.productID)
                 .map((productInfo) => (
                 <img
-                  src={`${requests.BACKEND_URL}/product/get_photo/${productInfo.productID}`}
+                  src={`${requests.BACKEND_URL}/product/get_photo/${productInfo.productID}?rand=${Date.now()}`}
                   key={productInfo.productID + currentProductID}
                   className={productInfo.productID == currentProductID
                       ? "product-info-products-article-product-active"
